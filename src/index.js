c7a3bcbc94d326b432050ade7096485981928f98
t_kinoshita <t_kinoshita@PD-90M-kinoshita.local>
2018-02-06 14:55:30 +0900

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import StaticMethodCall from "./todo";
-import Record from "./record";
+import $ from 'jquery';
+import 'slick-carousel';
+import 'slick/slick.css'
 
-var record = new Record('.item');
-document.addEventListener('DOMContentLoaded', record.init())
+export default class Carousel {
+    constructor(selector) {
+        this.$el = $(selector);
+        this.init();
+    }
 
-//module.exports = Rec;
+    init() {
+      console.log(this.$el)
+        this.$el.slick({
+            dots: true,
+            autoplay: true,
+              speed: 400,
+              dotsClass: 'dot-class',
+        });
+    }
+}
 
-
+var c = new Carousel('.slider')
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,24 @@ module.exports = {
         path:path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
+    resolve: {
+        extensions: ['.js', '.css'],
+        alias: {
+            slick: path.resolve(__dirname, 'node_modules/slick-carousel/slick/')
+        }
+      },
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.css$/, 
+                loaders: ['style-loader', 'css-loader?modules'],
+            },
+            { test: /\.(eot|svg|woff|ttf|gif)$/, loader: 'url-loader' }
         ]
     },
     devtool: 'inline-source-map'
